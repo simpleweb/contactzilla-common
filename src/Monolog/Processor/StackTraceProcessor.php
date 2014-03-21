61c1793cf8d73c9605a69a60a3e45c8ee0588b4f
Tom Holder <tom@simpleweb.co.uk>
2014-03-21 13:01:54 +0000

    Stack trace processor, all the call stack.

---
+++ src/Monolog/Processor/StackTraceProcessor.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Contactzilla\Common\Monolog\Processor;
+
+/**
+ * A monolog processor for adding in a stack trace to the extras
+ *
+ * @author Steve Lacey <steve@simpleweb.co.uk>
+ */
+class StackTraceProcessor
+{
+    public function __invoke(array $record)
+    {
+        $record['extra']['stack_trace'] = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 5);
+
+        return $record;
+    }
+}
+
