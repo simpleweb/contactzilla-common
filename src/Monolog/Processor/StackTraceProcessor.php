9c7c4fc77215ec1104b68e13136852399a02ba08
Steve Lacey <steve@simpleweb.co.uk>
2014-03-21 15:38:14 +0000

    Log stack traces for Contactzilla classes only.

--- src/Monolog/Processor/StackTraceProcessor.php
+++ src/Monolog/Processor/StackTraceProcessor.php
@@ -11,9 +11,25 @@ class StackTraceProcessor
 {
     public function __invoke(array $record)
     {
-        $record['extra']['stack_trace'] = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 10);
+        $record['extra']['stack_trace'] = implode(
+            ', ',
+            array_map(
+                function ($strace) {
+                    return $strace['class'] . $strace['type'] . $strace['function'];
+                },
+                array_filter(
+                    array_slice(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 50), 5),
+                    function ($strace) {
+                        return isset($strace['class']) && strpos($strace['class'], 'Contactzilla') === 0;
+                    }
+                )
+            )
+        );
+
+        if (!$record['extra']['stack_trace']) {
+            unset($record['extra']['stack_trace']);
+        };
 
         return $record;
     }
 }
-
